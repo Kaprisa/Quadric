15a06592621de4cad3c84fb4e280e433b8c84f7b
kaprisa57@gmail.com <kaprisa57@gmail.com>
2018-05-04 07:59:26 +0800

    video_height

--- resources/assets/js/components/Welcome.vue
+++ resources/assets/js/components/Welcome.vue
@@ -1,7 +1,7 @@
 <template>
    <!--<v-video course="common" video="overview.mov"></v-video>-->
    <!--<div>-->
-      <iframe  width="100%" height="800" style="margin: 0 auto;" src="https://www.youtube.com/embed/5A4oD61Atu8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      <iframe  width="100%" height="600" style="margin: 0 auto;" src="https://www.youtube.com/embed/5A4oD61Atu8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
    <!--</div>-->
 </template>
 
